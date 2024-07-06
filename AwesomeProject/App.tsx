c801164b9efda79f55c2722b707dfd818461f1d6
dev <dev@dev.com>
2024-07-06 13:55:04 +0800

    Demo2

--- AwesomeProject/App.tsx
+++ AwesomeProject/App.tsx
@@ -1,16 +1,32 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, TextInput, View } from "react-native";
 
-const MyApp = () => {
+const App = () => {
   return (
-    <View style={{
-      flex:1, 
-      justifyContent:"center", 
-      alignItems:"center"
-      }}>
-      <Text>Hello</Text>
-    </View>
+    <ScrollView>
+      <Text>Some text</Text>
+      <View>
+        <Text>Some more text</Text>
+        <Image
+          source={{
+            uri: "https://reactnative.dev/docs/assets/p_cat2.png"
+          }}
+          style={{
+            width: 200, height: 200
+          }}
+        />
+      </View>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1
+        }}
+        defaultValue="You can type in me"
+      />
+    </ScrollView>
+
   );
 }
 
-export default MyApp;
+export default App;
